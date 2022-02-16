3ae8b3a3ddf68ed044df2a11c059daed2583705b
Javed Baloch <mjavedahmed@live.com>
2022-02-16 17:42:32 +0500

    Models & Models

--- backend/controllers/goalController.js
+++ backend/controllers/goalController.js
@@ -1,36 +1,66 @@
+const { response } = require('express')
 const asyncHandler = require('express-async-handler')
+const req = require('express/lib/request')
+const { db } = require('../models/goalModel')
+
+const Goal = require('../models/goalModel')
 
 // @desc Get goals
 // @route GET /api/goals
 // @access private
 const getGoals = asyncHandler(async (req, res) => {
-    res.status(200).json({message: 'Get goals'})
+    const goals = await Goal.find()
+    res.status(200).json(goals)
 })
 
 // @desc Set goals
 // @route POST /api/goals
 // @access private
 const setGoals = asyncHandler(async (req, res) => {
-    // Check validation
-    if (!req.body.first_name) {
+    if (!req.body.text) {
         res.status(400)
         throw new Error('Please add a text field')
     }
-    res.status(200).json({message: 'Set goals'})
+
+    const goal = await Goal.create({text: req.body.text})
+
+    res.status(200).json(goal)
 })
 
 // @desc update goals
 // @route PUT /api/goals/:id
 // @access private
 const updateGoals = asyncHandler( async (req, res) => {
-    res.status(200).json({message: `Update goal ${req.params.id}`})
+    const goal = await Goal.findById(req.params.id)
+
+    if (!goal) {
+        res.status(400)
+        throw new Error('Goal not found')
+    }
+
+    const updatedGoal = await Goal.findByIdAndUpdate(req.params.id, req.body, { new: true})
+
+    res.status(200).json(updatedGoal)
 })
 
 // @desc delete goals
 // @route DELETE /api/goal/:id
 // @access private
 const deleteGoals = asyncHandler( async (req, res) => {
-    res.status(200).json({message: `Delete goal ${req.params.id}`})
+    // Find the user 
+    const goal = await Goal.findById(req.params.id)
+
+    // Error if not exists
+    if(!goal) {
+        return response(400)
+        throw new Error('Goal not fond')
+    }
+
+    // Delete the user
+    await goal.remove
+
+    // Respond back
+    res.status(200).json({message: `Deleted goal ${req.params.id}`})
 })
 
 module.exports = {
