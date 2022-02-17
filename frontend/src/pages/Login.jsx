078418d1388bcebc154cb57b6dd7cf57c6be28aa
Javed Baloch <mjavedahmed@live.com>
2022-02-17 12:52:01 +0500

    Login, Register components

--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -1,8 +1,67 @@
 import React from 'react'
+import { useState, useEffect } from 'react' 
+import {FaUser} from 'react-icons/fa'
 
 function Login() {
+  const [formData, setFormData] = useState({
+    email: '',
+    password: ''
+  })
+
+  // Distructure Form Data
+  const {email, password} = formData
+
+  // Funcs
+  const onSubmit = (e) => {
+    e.preventDefault()
+  }
+
+  const onChange = (e) => {
+    setFormData( ( prevState ) => ({
+      ...prevState,
+      [e.target.name]: e.target.value
+    }))
+  }
+
   return (
-    <div>Login</div>
+    <>
+      <section className='heading'>
+        <h1>
+          <FaUser /> Login
+        </h1>
+        <p>Please login to your account</p>
+      </section>
+
+      <section className="form">
+        <form onSubmit={onSubmit}>
+          <div className="form-group">
+            <input
+              type="email"
+              name="email"
+              className="form-control"
+              placeholder="Please enter username"
+              id="email"
+              value={email}
+              onChange={onChange}
+            />
+          </div>
+          <div className="form-group">
+            <input
+                type="password"
+                name="password"
+                className="form-control"
+                placeholder="Please enter password"
+                id="email"
+                value={password}
+                onChange={onChange}
+              />
+          </div>
+          <div className="form-group">
+            <button class="btn btn-block">Login</button>
+          </div>
+        </form>
+      </section>
+    </>
   )
 }
 
--- frontend/src/pages/Register.jsx
+++ frontend/src/pages/Register.jsx
@@ -1,8 +1,91 @@
 import React from 'react'
+import { useState, useEffect } from 'react' 
+import {FaUser} from 'react-icons/fa'
 
 function Register() {
+
+  // Keep all the fields in state
+  const [formData, setFormData] = useState({
+    name: '',
+    email: '',
+    password: '',
+    password2: ''
+  }) 
+
+  // Destructure fields
+  const {name, email, password, password2} = formData
+
+  // Funcs
+  const onChange = (e) => {
+    setFormData( (prevState) => ({
+      ...prevState,
+      [e.target.name]: e.target.value
+    }))
+  }
+
+  const onSubmit = (e) => {
+    e.preventDefault()
+  }
+
   return (
-    <div>Register</div>
+    <>
+      <section className='heading'>
+        <h1>
+          <FaUser /> Register
+        </h1>
+        <p>Please create an account</p>
+      </section>
+
+      <section className="form">
+        <form onSubmit={onSubmit}>
+          <div class="form-group">
+            <input 
+              type="text" 
+              className="form-control" 
+              id="name" 
+              name="name" 
+              value={name} 
+              placeholder="Enter your name" 
+              onChange={onChange} 
+            />
+
+            <input 
+              type="text" 
+              className="form-control" 
+              id="email" 
+              name="email" 
+              value={email} 
+              placeholder="Enter your email" 
+              onChange={onChange} 
+            />
+
+            <input 
+              type="password" 
+              className="form-control" 
+              id="password" 
+              name="password" 
+              value={password} 
+              placeholder="Enter your password" 
+              onChange={onChange} 
+            />
+
+            <input 
+              type="password2" 
+              className="form-control" 
+              id="password2" 
+              name="password2" 
+              value={password2} 
+              placeholder="Confirm Password" 
+              onChange={onChange} 
+            />
+          </div>
+
+          <div className="form-group">
+            <button type="submit" class="btn btn-block">Submit</button>
+          </div>
+        </form>
+      </section>
+    </>
   )
 }
 
